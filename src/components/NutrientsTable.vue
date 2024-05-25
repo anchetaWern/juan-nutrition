a64af9a25e94e610b5a7eb8468cd64366605ee8c
Wern Ancheta <ancheta.wern@gmail.com>
2024-05-25 15:46:10 +0800

    [improvement] use different colors for DV percentage consumed

--- src/components/NutrientsTable.vue
+++ src/components/NutrientsTable.vue
@@ -66,31 +66,51 @@ export default {
         return 0; 
       }
 
-      const getBackgroundColor = (value, reni_percent_value) => {
-        return value > reni_percent_value ? 'red-darken-1' : 'blue-darken-1';
+      const daily_value_colors = ['blue', 'orange', 'red'];
+
+      const getBackgroundColor = (value, daily_limit) => {
+        // DV percentages: 
+        /*
+        5% and below = blue
+        6 to 19% = orange
+        20% upwards = red
+        */
+        const dv_percent = calculatePercentage(value, daily_limit);
+        if (dv_percent >= 20) {
+          return 'red-darken-1';  
+        } else if (dv_percent >= 6 && dv_percent <= 19) {
+          return 'orange-darken-1';
+        }
+        return 'blue-darken-1';
       };
 
-      const getColor = (value, reni_percent_value) => {
-        return value > reni_percent_value ? 'red-darken-3' : 'blue-darken-3';
+      const getColor = (value, daily_limit) => {
+        const dv_percent = calculatePercentage(value, daily_limit);
+        if (dv_percent >= 20) {
+          return 'red-darken-3';  
+        } else if (dv_percent >= 6 && dv_percent <= 19) {
+          return 'orange-darken-3';
+        }
+        return 'blue-darken-3';
       };
 
-      const getReverse = (value, reni_percent_value) => {
-        return value > reni_percent_value ? true : false;
+      const getReverse = (value, daily_limit) => {
+        return value > daily_limit ? true : false;
       }
 
       const nutrientsWithPercentage = computed(() => {
         return props.nutrients.map(nutrient => {
           
-          const reni_percent_value = props.recommended_daily_values[nutrient.name]
+          const daily_limit = props.recommended_daily_values[nutrient.name]
           const percentage = calculateReniPercentage(nutrient.name, nutrient.amount);
           const hasRecommendedDailyValues = nutrients_with_recommended_daily_values.indexOf(nutrient.name) !== -1;
 
           return {
             ...nutrient,
             percentage,
-            bgColor: getBackgroundColor(nutrient.amount, reni_percent_value),
-            color: getColor(nutrient.amount, reni_percent_value),
-            reverse: getReverse(nutrient.amount, reni_percent_value), 
+            bgColor: getBackgroundColor(nutrient.amount, daily_limit),
+            color: getColor(nutrient.amount, daily_limit),
+            reverse: getReverse(nutrient.amount, daily_limit), 
             hasRecommendedDailyValues: hasRecommendedDailyValues,
             dailyLimit: hasRecommendedDailyValues && props.recommended_daily_values[nutrient.name] ? props.recommended_daily_values[nutrient.name].toFixed(0) : null,
           };
