e90fe75baa00dc81299e13625d25ecb470056349
Wern Ancheta <ancheta.wern@gmail.com>
2024-06-09 09:46:14 +0800

    [feature] auto-scroll to top when navigating between pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import SearchView from '../views/SearchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
   routes: [
     {
       path: '/',
