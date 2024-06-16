420c74343866b83bd95267825e32ea1182851dba
Wern Ancheta <ancheta.wern@gmail.com>
2024-06-16 15:23:55 +0800

    [improvement] include more details to help text

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -42,6 +42,15 @@
           <br><br>You can click on any of the food categories in the homepage. Note that triggering search while viewing a category will limit the search results to the current category. Be sure to navigate to the homepage before you initiate search if you don't want this behavior.
           <br><br>Results are sorted from smallest nutrition value to the largest. 
           <br><br>Only 10 food items is loaded per page. You can click the left and right arrows at the bottom of each page to navigate through the available pages.
+          <br><br>In the food page, you can see the nutritional values you'll typically get from consuming the specified serving size. 
+          <br>It is compared to the daily recommended values and is color-coded so you can easily see whether a food is dense with a specific nutrient. Red = high, orange = moderate, blue = low.
+          <br><br>The daily recommended values have two sources. It is primarily from DOST-FNRI's (Department of Science and Technology - Food and Nutrition Research Institute) Philippine Dietary Reference Intakes (PDRI). 
+          <br>This was previously called RENI or Recommended Energy and Nutrient Intakes. The terms are interchangeable provided most of the food labels in various food items today are still using the RENI 2002 edition. 
+          <br>The reference values for the daily limits are mostly using male, 19 to 29 years old. Though some food items such as powdered milk or baby food are using reference values from their respective target population.
+          <br><br>In cases where the daily value isn't available in PDRI, FDA's daily values are used: https://www.fda.gov/food/nutrition-facts-label/daily-value-nutrition-and-supplement-facts-labels
+          <br><br>Most of the food items here are from PhilFCT (Philippine Food Composition Tables Online Database) https://i.fnri.dost.gov.ph/fct/library/starting_pg
+          <br>Some are gathered from the author taking pictures of food labels in common grocery items. If you want to contribute, be sure to visit <a href="https://contribute.juanutrisyon.info" target="_blank">contribute.juanutrisyon.info</a>
+          and send me a message at ancheta dot wern at gmail dot com, as it requires an API key to submit food labels.
         </template>
 
         <v-card-actions>
