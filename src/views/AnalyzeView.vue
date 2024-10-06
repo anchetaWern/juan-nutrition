6f37ba622f36927ad858ccd91e9f6f9938b3c580
Wern Ancheta <ancheta.wern@gmail.com>
2024-10-06 18:25:49 +0800

    [fix] dont show summary if no foods added

--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -24,17 +24,19 @@
         </div>
       </div>
 
-      <div class="text-subtitle-1 mb-2">Summary</div>
+      <div v-if="analyze && analyze.length > 0">
+        <div class="text-subtitle-1 mb-2">Summary</div>
 
-      <v-row justify="space-between" no-gutters>
-        <v-col
-          v-for="summary_nut in summary_nutrients"
-          :key="summary_nut.name"
-          sm="3"
-        >
-          <NutritionCard :nutrient="summary_nut" :limits="recommended_daily_values" />
-        </v-col>
-      </v-row>
+        <v-row justify="space-between" no-gutters>
+          <v-col
+            v-for="summary_nut in summary_nutrients"
+            :key="summary_nut.name"
+            sm="4" md="4"
+          >
+            <NutritionCard :nutrient="summary_nut" :limits="recommended_daily_values" />
+          </v-col>
+        </v-row>
+      </div>
 
     </v-container>
     
