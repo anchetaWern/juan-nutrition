fabd2304f6a7e6dacc3f1747aa37c5601aa36cad
Wern Ancheta <ancheta.wern@gmail.com>
2024-10-13 18:51:31 +0800

    [improvement] add indicator arrows for nutrition card
    red up arrow for overconsume
    red down arrow for deficient
    orange up arrow for near over consumption

--- src/components/NutritionCard.vue
+++ src/components/NutritionCard.vue
@@ -2,19 +2,23 @@
 
   <v-card class="pt-0 pb-2 pl-0 pr-0 card text-center">
     <template v-slot:title>
-      <span :class="`text-body-1 font-weight-bold ${valueColor(nutrient.name, nutrient.amount)}`">{{ wholeNumber(nutrient.amount) }}{{ nutrient.unit }}</span>
+      <v-icon v-if="arrow(nutrient.name, nutrient.amount)" :icon="`${arrow(nutrient.name, nutrient.amount)}`" size="x-small" :color="`${arrowColor(nutrient.name, nutrient.amount)}`"></v-icon>
+      <span :class="`text-body-2 font-weight-bold ${valueColor(nutrient.name, nutrient.amount)}`">{{ wholeNumber(nutrient.amount) }}{{ nutrient.unit }}</span>
+      
     </template>
 
-    <v-card-text class="text-body-2 pa-1">
+
+    <v-card-text class="text-caption pa-1">
     {{ nutrient.name }}
     </v-card-text>
+
   </v-card>
 
 </template>
 
 <style scoped>
 .card {
-  width: 100px;
+  width: 110px;
 }
 </style>
 
@@ -39,6 +43,70 @@ export default {
   },
 
   setup(props) {
+
+    const arrowColor = (name, amount) => {
+      const limit = props.limits[name];
+
+      // deficient: down red arrow
+      // over-consumed: up red arrow
+      // good coverage: none
+
+      if (good_nutrients.includes(name)) {
+        if (amount <= limit) {
+          return 'red'; 
+        } else if (amount > limit && amount <= limit * 1.2) {
+          return ''; 
+        } else if (amount > limit * 1.2) {
+          return 'red'; 
+        }
+      }
+
+      if (bad_nutrients.includes(name)) {
+        const bufferAmount = limit * 0.2; 
+
+        if (amount > limit) {
+          return 'red'; 
+        } else if (amount > limit - bufferAmount) {
+          return 'orange'; 
+        } else {
+          return ''; 
+        }
+      }
+
+      return '';
+    }
+
+    const arrow = (name, amount) => {
+      const limit = props.limits[name];
+
+      // deficient: down red arrow
+      // over-consumed: up red arrow
+      // good coverage: none
+
+      if (good_nutrients.includes(name)) {
+        if (amount <= limit) {
+          return 'mdi-menu-down'; 
+        } else if (amount > limit && amount <= limit * 1.2) {
+          return ''; 
+        } else if (amount > limit * 1.2) {
+          return 'mdi-menu-up'; 
+        }
+      }
+
+      if (bad_nutrients.includes(name)) {
+        const bufferAmount = limit * 0.2; 
+
+        if (amount > limit) {
+          return 'mdi-menu-up'; 
+        } else if (amount > limit - bufferAmount) {
+          return 'mdi-menu-up'; 
+        } else {
+          return ''; 
+        }
+      }
+
+      return '';
+    }
 
     const valueColor = (name, amount) => {
       const limit = props.limits[name];
@@ -69,7 +137,9 @@ export default {
     };
 
     return {
-      valueColor
+      valueColor,
+      arrow,
+      arrowColor
     }
   },
 
--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -27,12 +27,13 @@
       <div v-if="analyze && analyze.length > 0">
         <div class="text-subtitle-1 mt-5 mb-2">Summary</div>
 
-        <v-row justify="space-between" dense>
+        <v-row justify="space-between" dense no-gutters>
           <v-col
             v-for="summary_nut in summary_nutrients"
             :key="summary_nut.name"
             sm="4" 
             md="4"
+            class="mb-1"
           >
             <NutritionCard :nutrient="summary_nut" :limits="recommended_daily_values" />
           </v-col>
