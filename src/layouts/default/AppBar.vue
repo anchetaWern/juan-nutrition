b9ff95bb5828d11834c6d860ac3ca435e674a878
Wern Ancheta <ancheta.wern@gmail.com>
2025-01-03 10:29:15 +0800

    [improvement] update help text

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -69,6 +69,7 @@
           <ul class="pt-1 pl-5">
             <li>Searching and viewing nutrient information of foods.</li>
             <li>Diet analysis.</li>
+            <li>Recipe analysis.</li>
           </ul>
           <br>
           To view food data, you can click on the magnifying glass icon to begin searching. Just type in what you're looking for (eg. <code>Rambutan</code>, <code>high protein</code>, <code>carbohydrates<=2g</code>, <code>high iron</code>, <code>high vitamin c</code>). 
@@ -77,14 +78,18 @@
           <br><br>Only 10 foods are loaded per page. You can click the left and right arrows at the bottom of each page to navigate through the available pages.
 
           <br><br>
-          To analyze your diet, search for the foods you ate for a day and click on the "Analyze" button. Repeat this step for every food you ate. Once done, click on the graph icon on the top portion of the screen then adjust the serving sizes for each food.<br>
+          To <a href="/analyze">analyze your diet</a>, search for the foods you ate for a day and click on the "Analyze" button. Repeat this step for every food you ate. Once done, click on the graph icon on the top portion of the screen then adjust the serving sizes for each food.<br>
           The summary section is where all the most important nutrients to watch out for is displayed. <br>
 
+          <br>
+          You can use the <a href="/recipe">recipe analysis</a> tool to analyze the nutrient content of a recipe. Recipe analysis works the same way as diet analysis: search for the individual ingredients and add them to a recipe. 
+          You will then be able to view the nutrient content of the recipe.
+          You can also save your recipe for other users to view and modify.  
+
+          <br>
           <br>Most of the foods here are from PhilFCT <a href="https://i.fnri.dost.gov.ph/fct/library/starting_pg" target="_blank">(Philippine Food Composition Tables Online Database)</a>
-          <br>Some are gathered from the author taking pictures of food labels. If you want to contribute, please visit <a href="https://contribute.juanutrisyon.info" target="_blank">contribute.juanutrisyon.info</a>
-          and send me a message at ancheta dot wern at gmail dot com, as it requires an API key to submit food labels.
-        
-          <br><br>Note that the food categorization still needs a lot of work. The foods are currently categorized via AI so it's not perfect. The categorization tool is still a work in progress.
+          <br>Some are gathered from the author taking pictures of food labels. If you want to contribute, <a href="/register">create an account</a> then visit <a href="https://contribute.juanutrisyon.info" target="_blank">contribute.juanutrisyon.info</a>.
+
         </template>
 
         <v-card-actions>
